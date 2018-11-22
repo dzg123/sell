3452dc81dac3f06c42f0771c6b323543b5b48e1e
dzg <1395224335@qq.com>
2018-11-22 11:27:01 +0800

    init project，使用日志logback

---
+++ src/test/java/com/dzg/sell/LogTest.java
@@ -0,0 +1,23 @@
+package com.dzg.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogTest {
+    @Test
+    public void test1(){
+        String name = "imooc";
+        String password = "123456";
+        log.debug("debug...");
+        log.info("name: " + name + " ,password: " + password);
+        log.info("name: {}, password: {}", name, password);
+        log.error("error...");
+        log.warn("warn...");
+    }
+}
