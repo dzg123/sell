9aa07a77a26c1bbbd6d38d9cba6f28fb7dabb07d
dzg <1395224335@qq.com>
2018-11-27 14:28:41 +0800

    产品信息jpa仓库

---
+++ src/main/java/com/dzg/sell/repository/ProductInfoRepository.java
@@ -0,0 +1,10 @@
+package com.dzg.sell.repository;
+
+import com.dzg.sell.domain.ProductInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductInfoRepository extends JpaRepository<ProductInfo,String> {
+    List<ProductInfo> findByProductStatus(Integer productStatus);
+}
